102f9ba290e9f6fd4fc574b632c11c3133de1be7
David Kolb <35406945+kolbeuk@users.noreply.github.com>
2020-01-02 18:44:47 +0000

    Create  weather and police.py

---
+++ weather and police.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+dfs = pd.read_csv('StormEvents_details-ftp_v1.0_d2015_c20191116.csv')
+dfp = pd.read_csv('police-tsopp-mi-QueryResult-2.csv')
+
+dfs = dfs[dfs['STATE'] == 'MICHIGAN']
+
+# prepare pivot table for strom events
+wevent = dfs.groupby(['MONTH_NAME', 'EVENT_TYPE'])['EVENT_TYPE'].agg(['count'])
+wevent.sort_values(by=['MONTH_NAME', 'EVENT_TYPE'], inplace=True, ascending=True)
+wevent = wevent.reset_index()
+wevent.pivot(index='EVENT_TYPE', columns = 'MONTH_NAME', values='count')
+
+# prepare pivot table for police violations
+dfp['MONTH_NAME'] = dfp['stop_date'].str[5:7]
+pevent = dfp.groupby(['MONTH_NAME', 'violation'])['violation'].agg(['count'])
+pevent.sort_values(by=['MONTH_NAME', 'violation'], inplace=True, ascending=True)
+pevent = pevent.reset_index()
+pevent.pivot(index='violation', columns = 'MONTH_NAME', values='count')
+
