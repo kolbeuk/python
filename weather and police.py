c8b12fb78876ce7d5ed0840f91860b22af73b1ac
David Kolb <35406945+kolbeuk@users.noreply.github.com>
2020-01-02 18:58:40 +0000

    Update weather and police.py

--- weather and police.py
+++ weather and police.py
@@ -4,13 +4,29 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
+# month mapping for pivot tables
+months={'April': '04',
+            'August': '08',
+            'December': '12',
+            'February': '02',
+            'January': '01',
+            'July': '07',
+            'June': '06',
+            'March': '03',
+            'May': '05',
+            'November': '11',
+            'October': '10',
+            'September': '09'}
+
 #data set from https://www.ncdc.noaa.gov › stormevents
 dfs = pd.read_csv('StormEvents_details-ftp_v1.0_d2015_c20191116.csv')
 
 #data set from data_world
 dfp = pd.read_csv('police-tsopp-mi-QueryResult-2.csv')
 
+# subset the dataset and change months to numbers
 dfs = dfs[dfs['STATE'] == 'MICHIGAN']
+dfs['MONTH_NAME']= dfs['MONTH_NAME'].map(months)
 
 # prepare pivot table for strom events
 wevent = dfs.groupby(['MONTH_NAME', 'EVENT_TYPE'])['EVENT_TYPE'].agg(['count'])
